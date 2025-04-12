c5a3b1a409f4feeba289fbd16ab1af6aa44cbe98
문규민 <nemesis1001@kakao.com>
2025-04-12 14:46:23 +0900

    Initial commit

---
+++ src/test/kotlin/com/tistory/kmmoon/stockchartkafkastreams/StockChartKafkaStreamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.kmmoon.stockchartkafkastreams.stockchartkafkastreams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockChartKafkaStreamsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
